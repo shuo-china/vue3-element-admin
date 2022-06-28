c5d27492490ab0b6347950cdd94852b4f43b815e
aaron <1020069668@qq.com>
2022-06-28 17:44:44 +0800

    feat: feat: feat: feat: feat: feat: feat: feat: feat: feat: 完成权限

---
+++ src/views/profile/components/ProjectCard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>项目介绍</div>
+</template>
+
+<script setup>
+import {} from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,9 +1,42 @@
 <template>
-  <div>
-    <input type="text" />
+  <div class="my-container">
+    <el-row>
+      <el-col :span="6">
+        <project-card class="project-card"></project-card>
+      </el-col>
+      <el-col :span="18">
+        <el-card>
+          <el-tabs v-model="activeName">
+            <el-tab-pane :label="$t('msg.profile.feature')" name="feature">
+              <feature />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter">
+              <chapter />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.author')" name="author">
+              <author />
+            </el-tab-pane>
+          </el-tabs>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import ProjectCard from './components/ProjectCard'
+import Feature from './components/Feature'
+import Chapter from './components/Chapter'
+import Author from './components/Author'
+import { ref } from 'vue'
 
-<style lang="scss"></style>
+const activeName = ref('feature')
+</script>
+
+<style lang="scss">
+.my-container {
+  .user-card {
+    margin-right: 20px;
+  }
+}
+</style>
